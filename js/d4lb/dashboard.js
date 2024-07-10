2bde91e1ec9caf6184b08c259debfab149e730dc
christyuda <chrisyuda@ulbi.ac.id>
2024-07-10 13:34:05 +0700

    feat  :perbaiki untuk d3lb url reponya

--- js/d4lb/dashboard.js
+++ js/d4lb/dashboard.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const baseApiUrl = "https://repoulbi-be.ulbi.ac.id/repoulbi/contents";
-  const repository = "d3mi";
+  const repository = "d3lb";
   const foldersToHide = [
     ".vscode",
     "assets",
