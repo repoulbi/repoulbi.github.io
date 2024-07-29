98d1de0d25f3b0da25643c3dc92d13a568b794ab
christyuda <chrisyuda@ulbi.ac.id>
2024-07-29 17:14:08 +0700

    feat : ganti uri

--- js/s2mlog/dashboard.js
+++ js/s2mlog/dashboard.js
@@ -444,8 +444,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const pdfUrl = encodeURIComponent(url);
-    // window.open(`view.html?pdfUrl=${pdfUrl}`, "_blank");
-    window.open("view/#" + { url }, "_blank");
+    window.open(`view/#${pdfUrl}`, "_blank");
+    // window.open("view/#" + { url }, "_blank");
   };
 
   window.fetchData(""); // Initial fetch
